7e5a2cedd04830a1f8b91890dc4f2117c2f390d1
unknown <ingdanynoriega@gmail.com>
2023-09-15 23:13:44 -0500

    Ready 02

--- 02.js
+++ 02.js
@@ -12,6 +12,16 @@ function stringMasLarga(strings) {
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
 
   // Tu código aca
+  var arr=strings[0]
+   var may=''
+  for(i=0;i<strings.length;i++){
+   
+      if(strings[i].length>arr.length)
+      arr=strings[i]
+ 
+    
+  }
+return arr
 }
 
 // No modifiques nada debajo de esta linea //
